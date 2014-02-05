4e0ae00f89f2e7345a904cb0d2bbbc57a24fb654
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-02-04 21:28:41 -0500

    Update movie_exercises_challenge.sql

--- movie_exercises_challenge.sql
+++ movie_exercises_challenge.sql
@@ -1,6 +1,6 @@
-# Question 1
-# For each movie, return the title and the 'rating spread', that is, the difference between highest and lowest ratings
-# given to that movie. Sort by rating spread from highest to lowest, then by movie title.
+/* Question 1
+   For each movie, return the title and the 'rating spread', that is, the difference between highest and lowest ratings
+   given to that movie. Sort by rating spread from highest to lowest, then by movie title. */
 
   SELECT a.title, b.ratingSpread
     FROM Movie AS a
