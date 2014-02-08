524a7b731d8250a28be313380a0423631ae418ee
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-02-07 22:01:24 -0500

    Update movie_exercises_core.sql

--- movie_exercises_core.sql
+++ movie_exercises_core.sql
@@ -25,7 +25,8 @@ SELECT title
                      FROM Rating)
 
 /* Question 4
-Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings with a NULL value for the date. */
+   Some reviewers didn't provide a date with their rating. Find the names of all reviewers who have ratings with a NULL
+   value for the date. */
 
 SELECT name
   FROM Reviewer
@@ -34,7 +35,8 @@ SELECT name
                 WHERE ratingDate IS NULL)
 
 /* Question 5 
-Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. */
+   Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and
+   ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. */
 
   SELECT name, title, stars, ratingDate
     FROM (SELECT a.name, b.stars, b.ratingDate, b.mID
@@ -46,10 +48,12 @@ Write a query to return the ratings data in a more readable format: reviewer nam
 ORDER BY name, title, stars
 
 /* Question 6
-For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer's name and the title of the movie. */
+   For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return 
+   the reviewer's name and the title of the movie. */
 
 /* Question 7
-For each movie that has at least one rating, find the highest number of stars that movie received. Return the movie title and number of stars. Sort by movie title. */
+   For each movie that has at least one rating, find the highest number of stars that movie received. Return the movie 
+   title and number of stars. Sort by movie title. */
 
   SELECT a.title, b.maxStars
     FROM Movie AS a
@@ -60,7 +64,8 @@ For each movie that has at least one rating, find the highest number of stars th
 ORDER BY a.title
 
 /* Question 8
-List movie titles and average ratings, from highest-rated to lowest-rated. If two or more movies have the same average rating, list them in alphabetical order. */
+   List movie titles and average ratings, from highest-rated to lowest-rated. If two or more movies have the same 
+   average rating, list them in alphabetical order. */
 
   SELECT a.title, b.avgStars
     FROM Movie AS a
@@ -71,7 +76,7 @@ List movie titles and average ratings, from highest-rated to lowest-rated. If tw
 ORDER BY b.avgStars DESC, a.title
 
 /* Question 9
-Find the names of all reviewers who have contributed three or more ratings. */
+   Find the names of all reviewers who have contributed three or more ratings. */
 
 SELECT a.name
   FROM Reviewer AS a
