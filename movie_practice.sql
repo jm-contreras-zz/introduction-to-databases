785c0cc9de5755f2a303814466dea99835af34ed
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-02-08 01:50:20 -0500

    Update movie_practice.sql

--- movie_practice.sql
+++ movie_practice.sql
@@ -9,3 +9,57 @@ SELECT name
          WHERE mID = (SELECT mID
                         FROM Movie
                        WHERE title = 'Gone with the Wind'))
+
+/* Question 2
+  For any rating where the reviewer is the same as the director of the movie, return the reviewer name, movie title, and
+  number of stars. */
+
+SELECT a.name, c.title, b.stars
+  FROM Reviewer AS a
+  JOIN Rating AS b
+    ON a.rID = b.rID
+  JOIN Movie AS c
+    ON b.mID = c.mID
+ WHERE a.name = c.director
+ 
+ /* Question 3
+    Return all reviewer names and movie names together in a single list, alphabetized. (Sorting by the first name of the
+    reviewer and first word in the title is fine; no need for special processing on last names or removing "The".) */ 
+
+SELECT title
+  FROM Movie
+ UNION
+SELECT name
+  FROM Reviewer
+
+/* Question 4
+   Find the titles of all movies not reviewed by Chris Jackson. */
+
+SELECT title
+  FROM Movie
+ WHERE mID NOT IN
+       (SELECT mID
+          FROM Rating
+         WHERE rID = (SELECT rID
+                        FROM Reviewer
+                       WHERE name = 'Chris Jackson'))
+
+/* Question 5
+   For all pairs of reviewers such that both reviewers gave a rating to the same movie, return the names of both
+   reviewers. Eliminate duplicates, don't pair reviewers with themselves, and include each pair only once. For each pair, 
+   return the names in the pair in alphabetical order. */
+
+
+
+/* Question 6
+   For each rating that is the lowest (fewest stars) currently in the database, return the reviewer name, movie title, and
+   number of stars. */ 
+
+SELECT a.name, c.title, b.stars
+  FROM Reviewer AS a
+  JOIN Rating AS b
+    ON a.rID = b.rID
+  JOIN Movie AS c
+    ON b.mID = c.mID
+ WHERE b.stars = (SELECT MIN(stars)
+                    FROM Rating)
