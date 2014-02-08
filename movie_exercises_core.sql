d9de03a42f63697a33fb7fa5d9bdcd6a29c594c7
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-02-07 21:59:59 -0500

    Update movie_exercises_core.sql

--- movie_exercises_core.sql
+++ movie_exercises_core.sql
@@ -33,7 +33,7 @@ SELECT name
                  FROM Rating
                 WHERE ratingDate IS NULL)
 
-/* Question 5
+/* Question 5 
 Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars. */
 
   SELECT name, title, stars, ratingDate
