7927760617a5b661e67e85ea542c6763509067fd
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-04-25 14:58:07 -0400

    Update movie_schema_data.sql

--- movie_schema_data.sql
+++ movie_schema_data.sql
@@ -1,43 +1,43 @@
 /* Delete the tables if they already exist */
-drop table if exists Movie;
-drop table if exists Reviewer;
-drop table if exists Rating;
+DROP TABLE IF EXISTS Movie;
+DROP TABLE IF EXISTS Reviewer;
+DROP TABLE IF EXISTS Rating;
 
 /* Create the schema for our tables */
-create table Movie(mID int, title text, year int, director text);
-create table Reviewer(rID int, name text);
-create table Rating(rID int, mID int, stars int, ratingDate date);
+CREATE TABLE Movie(mID INT, title TEXT, year INT, director TEXT);
+CREATE TABLE Reviewer(rID INT, name TEXT);
+CREATE TABLE Rating(rID INT, mID INT, stars INT, ratingDate DATE);
 
 /* Populate the tables with our data */
-insert into Movie values(101, 'Gone with the Wind', 1939, 'Victor Fleming');
-insert into Movie values(102, 'Star Wars', 1977, 'George Lucas');
-insert into Movie values(103, 'The Sound of Music', 1965, 'Robert Wise');
-insert into Movie values(104, 'E.T.', 1982, 'Steven Spielberg');
-insert into Movie values(105, 'Titanic', 1997, 'James Cameron');
-insert into Movie values(106, 'Snow White', 1937, null);
-insert into Movie values(107, 'Avatar', 2009, 'James Cameron');
-insert into Movie values(108, 'Raiders of the Lost Ark', 1981, 'Steven Spielberg');
+INSERT INTO Movie VALUES(101, 'Gone with the Wind', 1939, 'Victor Fleming');
+INSERT INTO Movie VALUES(102, 'Star Wars', 1977, 'George Lucas');
+INSERT INTO Movie VALUES(103, 'The Sound of Music', 1965, 'Robert Wise');
+INSERT INTO Movie VALUES(104, 'E.T.', 1982, 'Steven Spielberg');
+INSERT INTO Movie VALUES(105, 'Titanic', 1997, 'James Cameron');
+INSERT INTO Movie VALUES(106, 'Snow White', 1937, NULL);
+INSERT INTO Movie VALUES(107, 'Avatar', 2009, 'James Cameron');
+INSERT INTO Movie VALUES(108, 'Raiders of the Lost Ark', 1981, 'Steven Spielberg');
 
-insert into Reviewer values(201, 'Sarah Martinez');
-insert into Reviewer values(202, 'Daniel Lewis');
-insert into Reviewer values(203, 'Brittany Harris');
-insert into Reviewer values(204, 'Mike Anderson');
-insert into Reviewer values(205, 'Chris Jackson');
-insert into Reviewer values(206, 'Elizabeth Thomas');
-insert into Reviewer values(207, 'James Cameron');
-insert into Reviewer values(208, 'Ashley White');
+INSERT INTO Reviewer VALUES(201, 'Sarah Martinez');
+INSERT INTO Reviewer VALUES(202, 'Daniel Lewis');
+INSERT INTO Reviewer VALUES(203, 'Brittany Harris');
+INSERT INTO Reviewer VALUES(204, 'Mike Anderson');
+INSERT INTO Reviewer VALUES(205, 'Chris Jackson');
+INSERT INTO Reviewer VALUES(206, 'Elizabeth Thomas');
+INSERT INTO Reviewer VALUES(207, 'James Cameron');
+INSERT INTO Reviewer VALUES(208, 'Ashley White');
 
-insert into Rating values(201, 101, 2, '2011-01-22');
-insert into Rating values(201, 101, 4, '2011-01-27');
-insert into Rating values(202, 106, 4, null);
-insert into Rating values(203, 103, 2, '2011-01-20');
-insert into Rating values(203, 108, 4, '2011-01-12');
-insert into Rating values(203, 108, 2, '2011-01-30');
-insert into Rating values(204, 101, 3, '2011-01-09');
-insert into Rating values(205, 103, 3, '2011-01-27');
-insert into Rating values(205, 104, 2, '2011-01-22');
-insert into Rating values(205, 108, 4, null);
-insert into Rating values(206, 107, 3, '2011-01-15');
-insert into Rating values(206, 106, 5, '2011-01-19');
-insert into Rating values(207, 107, 5, '2011-01-20');
-insert into Rating values(208, 104, 3, '2011-01-02');
+INSERT INTO Rating VALUES(201, 101, 2, '2011-01-22');
+INSERT INTO Rating VALUES(201, 101, 4, '2011-01-27');
+INSERT INTO Rating VALUES(202, 106, 4, NULL);
+INSERT INTO Rating VALUES(203, 103, 2, '2011-01-20');
+INSERT INTO Rating VALUES(203, 108, 4, '2011-01-12');
+INSERT INTO Rating VALUES(203, 108, 2, '2011-01-30');
+INSERT INTO Rating VALUES(204, 101, 3, '2011-01-09');
+INSERT INTO Rating VALUES(205, 103, 3, '2011-01-27');
+INSERT INTO Rating VALUES(205, 104, 2, '2011-01-22');
+INSERT INTO Rating VALUES(205, 108, 4, NULL);
+INSERT INTO Rating VALUES(206, 107, 3, '2011-01-15');
+INSERT INTO Rating VALUES(206, 106, 5, '2011-01-19');
+INSERT INTO Rating VALUES(207, 107, 5, '2011-01-20');
+INSERT INTO Rating VALUES(208, 104, 3, '2011-01-02');
