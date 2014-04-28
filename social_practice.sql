ababa28fb623db344e789476fbea75a0f1fecd21
Juan Manuel Contreras <juan.manuel.contreras.87@gmail.com>
2014-04-28 13:21:47 -0400

    Update social_practice.sql

--- social_practice.sql
+++ social_practice.sql
@@ -1,13 +1,13 @@
 /* Question 1
-   For every situation where student A likes student B, but we have no information about whom B likes (that is, B does     not appear as an ID1 in the Likes table), return A and B's names and grades. */
+   For every situation where student A likes student B, but we have no information about whom B likes (that is, B does       not appear as an ID1 in the Likes table), return A and B's names and grades. */
 
 SELECT a.Name, a.Grade, b.Name, b.Grade
   FROM Likes, Highschooler AS a, Highschooler AS b
  WHERE ID1 = a.ID AND ID2 = b.ID AND ID2 NOT IN (SELECT ID1
-                     FROM Likes)
+                                                   FROM Likes)
 
 /* Question 2
-   For every situation where student A likes student B, but student B likes a different student C, return the names and    grades of A, B, and C. */
+   For every situation where student A likes student B, but student B likes a different student C, return the names and      grades of A, B, and C. */
 
 SELECT c.Name, c.Grade, d.Name, d.Grade, e.Name, e.Grade
   FROM Likes AS a, Likes AS b, Highschooler AS c, Highschooler AS d, Highschooler AS e
@@ -19,5 +19,5 @@ SELECT c.Name, c.Grade, d.Name, d.Grade, e.Name, e.Grade
 SELECT Name, Grade
   FROM Highschooler
  WHERE ID NOT IN (SELECT DISTINCT(ID1)
-				    FROM Friend, Highschooler AS a, Highschooler AS b
+                    FROM Friend, Highschooler AS a, Highschooler AS b
                    WHERE ID1 = a.ID AND ID2 = b.ID AND a.Grade = b.Grade)
